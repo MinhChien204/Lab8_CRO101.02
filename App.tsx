9f41aa1e616cb01998c289ed5907a382e111bbea
MinhChien204 <cminh5344@gmail.com>
2024-04-10 09:41:51 +0700

    update lab 8

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import L11 from './L1/L11'
+import L22 from './L1/L22'
+import Dem from './Demo2/LT21'
+import MyComponent from './Demo2/LT22'
+import LT23 from './Demo2/LT23'
+import ParentComponent from './Demo2/LT24'
+import Main from './Demo2/L21/Main'
+import Header from './Demo2/L21/Header'
+import Footer from './Demo2/L21/Footer'
+import Body from './Demo2/L21/Body'
+import CRO31 from './L3/L31'
+import L32 from './L3/L32'
+import L33 from './L3/L33'
+import NgheNhac from './Demo4/Demo41/NgheNhac'
+import ChupAnh from './Demo4/Demo41/ChupAnh'
+import AppDemo5 from './Demo5/AppDemo5'
+import App61 from './Demo6/App61'
+import Demo71 from './L7/Demo71'
+import App72 from './L7/CRUDApp72'
+const App = () => {
+  return (
+    <View style={{
+      flex: 1
+    }}>
+      <App72/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
